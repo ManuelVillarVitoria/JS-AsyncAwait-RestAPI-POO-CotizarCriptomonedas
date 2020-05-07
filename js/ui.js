e5240a2469056f10983dba96b61db0c83b65843b
manu <manu.villar.vitoria@gmail.com>
2020-05-07 19:44:54 +0200

    building the 'Select' options

--- js/ui.js
+++ js/ui.js
@@ -11,7 +11,7 @@ class Interfaz {
     construirSelect() {
         cotizador.obtenerMonedasAPI()
             .then(monedas => {
-                console.log(monedas);
+                //console.log(monedas);
 
                 //console.log(monedas.monedas.Data.forEach(moneda => {
                     //console.log(moneda)// No podemos usar el método 'forEach' porque no tenemos un array
@@ -20,10 +20,19 @@ class Interfaz {
                 //console.log(Object.entries(monedas.monedas.Data));// Tomamos los objetos y los convertimos 
                                                                    //en arrays con 'Object.entries'
 
-                // Recorremos los arrays que obtenemos al transformar el objeto anidado con 'Object.entries'
+                //crear un selct de opciones                                               
+                const select = document.querySelector('#criptomoneda');
+
+                // Iteramos los arrays que obtenemos de la API, al transformar el objeto anidado con 'Object.entries'
                 for(const [key,value] of Object.entries(monedas.monedas.Data)) {
-                    console.log(key);//Obtenemos el nombre de las llaves de los arrays
-                    console.log(value);//Obtenemos el valorde los arrays
+                    //console.log(key);//Obtenemos el nombre de las llaves de los arrays
+                    //console.log(value);//Obtenemos el valorde los arrays
+
+                    //Añadir el Symbol y el Nombre como opciones
+                    const opcion = document.createElement('option');
+                    opcion.value = value.Symbol;
+                    opcion.appendChild(document.createTextNode(value.CoinName));
+                    select.appendChild(opcion);
                 }
             })      
     }
